0a7e34159991a1af5b6c34d45b71019c10371e68
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-01-23 15:01:59 +0900

    フォーム入力内容をリアルタイムに表示

--- app/src/main/java/com/android/bidirectionaldatabindingkotlin/ViewModel.kt
+++ app/src/main/java/com/android/bidirectionaldatabindingkotlin/ViewModel.kt
@@ -12,16 +12,21 @@ class ViewModel : BaseObservable() {
             // fieldはformTextのこと
             // フォーム入力内容がformTextにセットされる
             field = value
+            // View側にrealTimeTextの変更を通知(getRealTimetext()を呼ぶ)
+            notifyPropertyChanged(BR.realTimeText)
             // View側にbuttonEnableの変更を通知(isButtonEnable()を呼ぶ)
             notifyPropertyChanged(BR.buttonEnable)
         }
+
+    // フォーム入力内容をフォーム下のTextViewに反映する getter
+    @Bindable fun getRealTimeText(): String {
+        // return formText でも良いがわかりやすく一旦変数に代入してから return している
+        val realTimeText = formText
+        return realTimeText
+    }
 
     // formTextの値の有無によってボタン活性・非活性のフラグを返す
     @Bindable fun isButtonEnable(): Boolean {
         return !formText.isNullOrBlank()
     }
-
-    // これだとボタンに活性・非活性が通知されなかった。なんでだろう？
-    // @get:Bindable
-    // var buttonEnable: Boolean = !formText.isNullOrBlank()
 }
