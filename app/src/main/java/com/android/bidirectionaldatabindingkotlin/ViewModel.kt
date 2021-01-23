591cd0360b0c0007c25b787e429720e3e4ba13b5
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-01-23 14:48:34 +0900

    フォーム入力でボタンアクティブ

--- app/src/main/java/com/android/bidirectionaldatabindingkotlin/ViewModel.kt
+++ app/src/main/java/com/android/bidirectionaldatabindingkotlin/ViewModel.kt
@@ -1,8 +1,29 @@
 package com.android.bidirectionaldatabindingkotlin
 
 import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
 
 class ViewModel : BaseObservable() {
-    // ここにロジックを書いていく
 
+    // これでフォーム入力内容(formText)の getterとsetter が同時にセットできる
+    @get:Bindable
+    var formText: String = ""
+        set(value) {
+            // fieldはformTextのこと
+            // フォーム入力内容がformTextにセットされる
+            field = value
+            // View側にformTextの変更を通知
+            notifyPropertyChanged(BR.formText)
+            // View側にbuttonEnableの変更を通知(isButtonEnable()を呼ぶ)
+            notifyPropertyChanged(BR.buttonEnable)
+        }
+
+    // formTextの値の有無によってボタン活性・非活性のフラグを返す
+    @Bindable fun isButtonEnable(): Boolean {
+        return !formText.isNullOrBlank()
+    }
+
+    // これだとボタンに活性・非活性が通知されなかった。なんでだろう？
+    // @get:Bindable
+    // var buttonEnable: Boolean = !formText.isNullOrBlank()
 }
